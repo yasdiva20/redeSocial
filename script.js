415f0d4533a0a9d0cb77ad97c444e39af68433c5
yasdiva20 <yasmindesouzasilva0@gmail.com>
2026-09-09 11:35:23 -0300

    ok

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+    if (!likeBtn) return;
+
+    const likesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+    let isLiked = false;
+    let baseLikes = 0;
+
+    //texto inicial
+
+    if(likesCountSpan){
+        
+    }
+}
+
+)
